a3dad9fd1a42a292c97887aa68a42cd8acbc5aea
Patrick van Batenburg <zammypatrick@gmail.com>
2015-03-02 16:58:15 +0100

    Lesson Using Polymorphism

--- Exam 98 -372/Lesson01/Lesson01/Program.cs
+++ Exam 98 -372/Lesson01/Lesson01/Program.cs
@@ -4,49 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson1
+namespace Lesson01
 {
-    class Rectangle
+    class Program
     {
-        private double length;
-        private double width;
-
-        public double Length
-        {
-            get
-            {
-                return length;
-            }
-            set
-            {
-                if (value > 0.0)
-                    length = value;
-            }
-        }
-        public double Width
-        {
-            get
-            {
-                return width;
-            }
-            set
-            {
-                if (value > 0.0)
-                width = value;
-            }
-        }
-        public double GetArea()
-        {
-            return length * width;
-        }
-
         static void Main(string[] args)
         {
-            Rectangle rect = new Rectangle();
-            rect.Length = 10.0;
-            rect.Width = 20.0;
-            double area = rect.GetArea();
-            Console.WriteLine("Area of Rectangle: {0}", area);
+
+            List<Polygon> polygons = new List<Polygon>();
+            polygons.Add(new Polygon());
+            polygons.Add(new Rectangle());
+            polygons.Add(new Triangle());
+
+            foreach (Polygon p in polygons)
+            {
+                p.Draw();
+            }
         }
     }
 }
