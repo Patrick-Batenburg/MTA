3747b8cbcfe0dec785f0e42eead453c3f244d1e5
Patrick van Batenburg <zammypatrick@gmail.com>
2015-03-09 16:56:29 +0100

    lesson02 Understanding Contravariant and Covariance

---
+++ Exam 98 -372/Lesson02/SingleDimensionalArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[4] { 11, 7, 50, 45 };
+            Console.WriteLine("Array items: ");
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nMax number: {0}", numbers.Max());
+
+            Console.WriteLine("\nArray items in reverse: ");
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exam 98 -372/Lesson02/UsingArrayList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace UsingArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("Monday");
+            list.Add("Tuesday");
+            list.Add("Wednesday");
+            list.Add("Thursday");
+            list.Add("Friday");
+            list.Add("Saturday");
+            list.Add("Sunday");
+
+            Console.WriteLine("Original list: ");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            list.Sort();
+            Console.WriteLine("\nSorted list");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nItem Count: {0}", list.Count);
+            Console.ReadKey();
+        }
+    }
+}
