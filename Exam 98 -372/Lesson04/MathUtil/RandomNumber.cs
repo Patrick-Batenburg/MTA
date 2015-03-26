3b006d911de025bc818d41349df529a30ae70c56
Patrick van Batenburg <zammypatrick@gmail.com>
2015-03-26 12:37:51 +0100

    lesson04 1

---
+++ Exam 98 -372/Lesson04/MathUtil/RandomNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathUtil
+{
+    public class RandomNumber
+    {
+        public int MinValue { get; set; }
+        public int MaxValue { get; set; }
+
+        public RandomNumber()
+        {
+            MinValue = int.MinValue;
+            MaxValue = int.MinValue;
+        }
+
+        public int Get()
+        {
+            Random r = new Random();
+            return r.Next(MinValue, MaxValue);
+        }
+    }
+}
