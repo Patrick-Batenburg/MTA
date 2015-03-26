7d6631b41a6ee218965f3f440935640bd2177654
Patrick van Batenburg <zammypatrick@gmail.com>
2015-03-26 10:11:19 +0100

    skill summary lesson03

---
+++ Exam 98 -372/Lesson03/Skill Summary/Competency Assessment/Project 3-2/Project 3-2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace Project_3_2
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.MouseClick += (s, e) => MessageBox.Show(String.Format("Mouse Clicked at X: {0} Y: {1}", e.X, e.Y));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Exam 98 -372/Lesson03/Skill Summary/Proficiency Assessment/Project 3-3/Project 3-3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the divide calc.");
+            while (true)
+            {
+                decimal x;
+                decimal y;
+                Console.WriteLine("Enter the first set of number(s)");
+                decimal.TryParse(Console.ReadLine(), out x);
+
+                Console.WriteLine("Enter the second set of number(s)");
+                decimal.TryParse(Console.ReadLine(), out y);
+
+                try
+                {
+                    Console.WriteLine("Dividing {0} by {1} is {2}.", x, y, x / y);
+                }
+                catch (DivideByZeroException dbzex)
+                {
+                    Console.WriteLine(dbzex.Message);
+                }
+                catch (NullReferenceException nrex)
+                {
+                    Console.WriteLine(nrex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("\nDo you want to continue? (y/n)");
+                string answer = Console.ReadLine();
+                if (answer == "n")
+                {
+                    break;
+                }
+                else if (answer == "y")
+                {
+                    Console.WriteLine();
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Answer was incorrect, you will exit the program.");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
